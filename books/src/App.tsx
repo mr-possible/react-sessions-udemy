5a3d60184e2011a68937688c7af35425a177994e
mr-possible <sambhavd4@gmail.com>
2024-05-02 21:33:11 +0530

    section 7 (part 2 of section 6)

--- books/src/App.tsx
+++ books/src/App.tsx
@@ -1,36 +1,56 @@
 import 'bulma/css/bulma.css'
 import './css/App.css'
 
-import { ReactElement, useState } from "react"
+import { ReactElement, useEffect, useState } from "react"
 import BookCreate from "./components/BookCreate"
 import BookList from "./components/BookList"
 import Book from "./interfaces/Book"
+import axios, { AxiosResponse } from 'axios'
 
 function App(): ReactElement {
   const [books, setBooks] = useState<Book[]>([])
 
-  function createBook(newTitle: string) {
-    const updatedBooks = [
-      ...books,
+  useEffect(() => {
+    getAllBooks()
+  }, [])
+
+  async function getAllBooks() {
+    const response: AxiosResponse<Book[]> = await axios.get('http://localhost:3001/books')
+    setBooks(response.data)
+  }
+
+  async function createBook(newTitle: string) {
+    const response: AxiosResponse<Book> = await axios.post('http://localhost:3001/books',
       {
         id: Math.round(Math.random() * 9999),
         title: newTitle
       }
+    )
+    const updatedBooks = [
+      ...books,
+      response.data
     ]
     setBooks(updatedBooks);
   }
 
-  function deleteBookById(id: number) {
+  async function deleteBookById(id: number) {
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
       return id !== book.id
     })
     setBooks(updatedBooks)
   }
 
-  function updateBookTitle(id: number, newTitle: string) {
+  async function updateBookTitle(id: number, newTitle: string) {
+    const response: AxiosResponse<Book> = await axios.put(`http://localhost:3001/books/${id}`,
+      {
+        title: newTitle
+      }
+    )
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };   // VERY IMPORTANT PART HERE!! - take all diff new key-value pairs and add to the object to be sent.
       }
       return book;
     });
